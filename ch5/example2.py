0f982c33a411cb2330737425bbfccc7991d93b73
KrisNguyen135 <nguyenminhquan135@gmail.com>
2018-07-10 20:16:18 -0600

    update chapter 5

--- ch5/example2.py
+++ ch5/example2.py
@@ -4,6 +4,9 @@ url = 'http://www.thesaurus.com/browse/pythonic'
 
 r = requests.get(url)
 
+print(r.status_code)
+print(r.headers)
+
 with open('pythonic_thesaurus.html', 'w') as f:
     f.write(r.text)
 
