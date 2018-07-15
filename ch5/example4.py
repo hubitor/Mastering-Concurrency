ef0b958d2c01f58d20c533b5956a85b5ff50d1e5
KrisNguyen135 <nguyenminhquan135@gmail.com>
2018-07-14 19:21:23 -0600

    update chapter 5

---
+++ ch5/example4.py
@@ -0,0 +1,34 @@
+import requests
+from bs4 import BeautifulSoup
+
+def get_html_source(url):
+    try:
+        r = requests.get(url)
+
+        return r.text
+
+    except Exception as e:
+        print('There was a problem: %s.' % e)
+        return False
+
+def get_synonyms_from_html(html_source):
+    soup = BeautifulSoup(html_source, 'html.parser')
+    html_synonyms = soup.select('div.MainContentContainer > div > section > ul > li > span > a')
+
+    return [item.getText() for item in html_synonyms]
+
+root_url = 'http://www.thesaurus.com/browse/'
+
+while True:
+    print('Ctrl + C to quit.')
+
+    my_input = input('Enter a word for scraped synonyms: ').strip()
+    html_source = get_html_source(root_url + my_input)
+
+    if html_source:
+        synonyms = get_synonyms_from_html(html_source)
+        if len(synonyms):
+            print('%i synonyms found for %s:' % (len(synonyms), my_input))
+            print(synonyms)
+        else:
+            print('No synonym found for %s.' % my_input)
