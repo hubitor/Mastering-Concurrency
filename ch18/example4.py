6aa297a099995b5de25cf9c772cf3d0cfc305793
KrisNguyen135 <nguyenminhquan135@gmail.com>
2018-10-26 15:20:42 -0400

    chapter 18: update generator example

--- ch18/example4.py
+++ ch18/example4.py
@@ -2,8 +2,12 @@
 
 def read_data():
     for i in range(5):
+        print('Inside the inner for loop...')
         yield i * 2
 
 result = read_data()
 for i in range(6):
+    print('Inside the outer for loop...')
     print(next(result))
+
+print('Finished.')
