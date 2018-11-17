6f0562413c12483f3d2dca71112a1e181b714713
KrisNguyen135 <nguyenminhquan135@gmail.com>
2018-11-17 16:17:53 -0500

    chapter 5: added multithreaded web requests code

---
+++ ch05_v2/example3.py
@@ -0,0 +1,32 @@
+import threading
+import queue
+import requests
+
+class MyThread(threading.Thread):
+    def __init__(self, url):
+        threading.Thread.__init__(self)
+        self.url = url
+        self.result = None
+
+    def run(self):
+        res = requests.get(self.url)
+        self.result = f'{self.url}: {res.text}'
+
+urls = [
+    'http://httpstat.us/200',
+    'http://httpstat.us/400',
+    'http://httpstat.us/404',
+    'http://httpstat.us/408',
+    'http://httpstat.us/500',
+    'http://httpstat.us/524'
+]
+
+threads = [MyThread(url) for url in urls]
+for thread in threads:
+    thread.start()
+for thread in threads:
+    thread.join()
+for thread in threads:
+    print(thread.result)
+
+print('Done.')
---
+++ ch05_v2/example4.py
@@ -0,0 +1,29 @@
+import threading
+import queue
+import requests
+
+class MyThread(threading.Thread):
+    def __init__(self, url):
+        threading.Thread.__init__(self)
+        self.url = url
+        self.result = None
+
+    def run(self):
+        res = requests.get(self.url)
+        self.result = f'{self.url}: {res.text}'
+
+urls = [
+    'http://httpstat.us/200',
+    'http://httpstat.us/200?sleep=20000',
+    'http://httpstat.us/400'
+]
+
+threads = [MyThread(url) for url in urls]
+for thread in threads:
+    thread.start()
+for thread in threads:
+    thread.join()
+for thread in threads:
+    print(thread.result)
+
+print('Done.')
